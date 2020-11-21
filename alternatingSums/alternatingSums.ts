5e9b4788dccf9dd12fe47ecd042504016f9f1eea
Luis Salas <luisfernandosalasg@gmail.com>
2020-11-21 05:07:04 -0500

    -

--- alternatingSums/alternatingSums.ts
+++ alternatingSums/alternatingSums.ts
@@ -1,3 +1,16 @@
-function alternatingSums(a: number[]): any[] {}
+function alternatingSums(a: number[]): number[] {
+	let evenSum = 0;
+	let oddSum = 0;
+
+	a.forEach((ele, index) => {
+		if (index % 2 === 0) {
+			evenSum += ele;
+		} else {
+			oddSum += ele;
+		}
+	});
+
+	return [evenSum, oddSum];
+}
 
 console.log(alternatingSums([50, 60, 60, 45, 70]));
