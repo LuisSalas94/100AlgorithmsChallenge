68f8fad39b8b842259ebc2346c167bc668085b85
Luis Salas <luisfernandosalasg@gmail.com>
2020-11-21 06:31:55 -0500

    -

--- arrayReplace/arrayReplace.ts
+++ arrayReplace/arrayReplace.ts
@@ -1,5 +1,15 @@
-function arrayReplace(inputArray: number[], elemToReplace: number, substitutionElem: number): number[] {
-   
+function arrayReplace(
+	inputArray: number[],
+	elemToReplace: number,
+	substitutionElem: number
+): number[] {
+	inputArray.forEach((ele, index) => {
+		if (ele === elemToReplace) {
+			inputArray[index] = substitutionElem;
+		}
+	});
+
+	return inputArray;
 }
 
 console.log(arrayReplace([1, 2, 1], 1, 3));
