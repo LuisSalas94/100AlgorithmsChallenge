755675520e31390e63f6826457c3e83e0ce56e96
Luis Salas <luisfernandosalasg@gmail.com>
2020-11-21 07:51:34 -0500

    -

--- checkPalindrome/checkPalindrome.ts
+++ checkPalindrome/checkPalindrome.ts
@@ -1,7 +1,8 @@
 function checkPalindrome(inputString: string): boolean {
-
+	let reversed = inputString.toLowerCase().split("").reverse().join("");
+	return reversed === inputString;
 }
 
-console.log(checkPalindrome('aabaa'));
-console.log(checkPalindrome('abac'));
-console.log(checkPalindrome('a'));
+console.log(checkPalindrome("aabaa"));
+console.log(checkPalindrome("abac"));
+console.log(checkPalindrome("a"));
