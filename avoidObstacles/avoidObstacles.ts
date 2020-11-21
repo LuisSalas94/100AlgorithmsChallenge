761868546e2a0ada7a095a3f1e323d0a4b5adda7
Luis Salas <luisfernandosalasg@gmail.com>
2020-11-21 06:46:09 -0500

    -

--- avoidObstacles/avoidObstacles.ts
+++ avoidObstacles/avoidObstacles.ts
@@ -1,5 +1,15 @@
 function avoidObstacles(inputArray: number[]): number {
+	inputArray = inputArray.sort((a, b) => {
+		return a - b;
+	});
 
+	const largestArrayVal = inputArray[inputArray.length - 1];
+
+	for (let i = 1; i <= largestArrayVal + 1; i++) {
+		if (inputArray.every((ele) => ele % i !== 0)) {
+			return i;
+		}
+	}
 }
 
 console.log(avoidObstacles([5, 3, 6, 7, 9]));
