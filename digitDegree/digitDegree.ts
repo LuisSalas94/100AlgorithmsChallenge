954e013c384fabaaa8bbaeccca677b9b4ca9c6d2
Luis Salas <luisfernandosalasg@gmail.com>
2020-11-23 06:35:29 -0500

    -

--- digitDegree/digitDegree.ts
+++ digitDegree/digitDegree.ts
@@ -1,5 +1,30 @@
 function digitDegree(n: number): number {
-  
+	let count = 0;
+	let currentNumber = n;
+
+	if (n <= 9) {
+		return count;
+	} else {
+		while (true) {
+			count++;
+			currentNumber = getDigit(currentNumber);
+			if (currentNumber <= 9) {
+				break;
+			}
+		}
+	}
+	return count;
+}
+
+function getDigit(num) {
+	const nums = num
+		.toString()
+		.split("")
+		.map((element) => parseInt(element));
+
+	return nums.reduce((a, b) => {
+		return a + b;
+	});
 }
 
 console.log(digitDegree(5));
