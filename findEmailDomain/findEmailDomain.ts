2d9774a734187aa8838cfed2647d3b416300d55c
Luis Salas <luisfernandosalasg@gmail.com>
2020-11-24 06:02:49 -0500

    -

--- findEmailDomain/findEmailDomain.ts
+++ findEmailDomain/findEmailDomain.ts
@@ -1,6 +1,14 @@
-function findEmailDomain(address: string): string {
+/* 
+The lastIndexOf() method returns the index within the calling String object of the last occurrence of the specified value, searching backwards from fromIndex. Returns -1 if the value is not found.
 
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/lastIndexOf
+*/
+
+function findEmailDomain(address: string): string {
+	const lastIndex = address.lastIndexOf("@");
+
+	return address.slice(lastIndex + 1, address.length);
 }
 
-console.log(findEmailDomain('prettyandsimple@example.com'));
-console.log(findEmailDomain('<>[]:,;@\"!#$%&*+-/=?^_{}| ~.a\"@example.org'));
+console.log(findEmailDomain("prettyandsimple@example.com"));
+console.log(findEmailDomain('<>[]:,;@"!#$%&*+-/=?^_{}| ~.a"@example.org'));
