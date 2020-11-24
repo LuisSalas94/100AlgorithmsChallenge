fb1ac8059136d4de910ae5970b219f88b62ec38d
Luis Salas <luisfernandosalasg@gmail.com>
2020-11-24 05:17:49 -0500

    -

--- extractMatrixColumn/extractMatrixColumn.ts
+++ extractMatrixColumn/extractMatrixColumn.ts
@@ -1,5 +1,20 @@
 function extractMatrixColumn(matrix: number[][], column: number): number[] {
+	const matrixCoumn = [];
 
+	matrix.forEach((row) => {
+		return matrixCoumn.push(row[column]);
+	});
+
+	return matrixCoumn;
 }
 
-console.log(extractMatrixColumn([[1, 1, 1, 2], [0, 5, 0, 4], [2, 1, 3, 6]], 2));
+console.log(
+	extractMatrixColumn(
+		[
+			[1, 1, 1, 2],
+			[0, 5, 0, 4],
+			[2, 1, 3, 6],
+		],
+		2
+	)
+);
