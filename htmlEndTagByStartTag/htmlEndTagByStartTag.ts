a4089ea900812b522e1e85a2e58977e39471b0e2
Luis Salas <luisfernandosalasg@gmail.com>
2020-11-25 05:55:20 -0500

    -

--- htmlEndTagByStartTag/htmlEndTagByStartTag.ts
+++ htmlEndTagByStartTag/htmlEndTagByStartTag.ts
@@ -1,6 +1,20 @@
 function htmlEndTagByStartTag(startTag: string): string {
+	const splitString = startTag.split(" ");
+	const splitTag = splitString[0].toString().split("");
+	let endTag = "</";
 
+	for (let i = 1; i < splitTag.length; i++) {
+		endTag += splitTag[i];
+	}
+
+	if (endTag[endTag.length - 1] === ">") {
+		endTag;
+	} else {
+		endTag += ">";
+	}
+
+	return endTag;
 }
 
 console.log(htmlEndTagByStartTag("<button type='button' disabled>"));
-console.log(htmlEndTagByStartTag('<i>'))
+console.log(htmlEndTagByStartTag("<i>"));
