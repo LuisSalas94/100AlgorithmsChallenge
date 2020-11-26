d7a98745493a772d039d2c8033e569a6119600f5
Luis Salas <luisfernandosalasg@gmail.com>
2020-11-26 05:43:29 -0500

    -

--- missingLetters/missingLetters.ts
+++ missingLetters/missingLetters.ts
@@ -1,5 +1,13 @@
 function missingLetters(str: string): string {
-   
+	const alphabet = "abcdefghijklmnopqrstuvwxyz";
+	const chars = str.split("");
+
+	for (let i = 0; i < chars.length; i++) {
+		if (chars[i] !== alphabet[i]) {
+			return alphabet[i];
+		}
+	}
+	return undefined;
 }
 console.log(missingLetters("bce"));
 console.log(missingLetters("abce"));
