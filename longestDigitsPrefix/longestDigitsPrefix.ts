579078abd835ec85f652b2cfd908170a5be300ec
Luis Salas <luisfernandosalasg@gmail.com>
2020-11-26 05:19:00 -0500

    -

--- longestDigitsPrefix/longestDigitsPrefix.ts
+++ longestDigitsPrefix/longestDigitsPrefix.ts
@@ -1,3 +1,21 @@
-function longestDigitsPrefix(inputString: string): string {}
+/* 
+The includes() method determines whether an array includes a certain value among its entries, returning true or false as appropriate.
+
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/includes
+*/
+
+function longestDigitsPrefix(inputString: string): string {
+	let prefix = "";
+
+	for (let char of inputString) {
+		const num = parseInt(char);
+		if (isNaN(num)) {
+			break;
+		}
+		prefix += char;
+	}
+
+	return prefix;
+}
 
 console.log(longestDigitsPrefix("123aa1"));
