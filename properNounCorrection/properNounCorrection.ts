be619ee2b99c8436bd72c0b22526a1fe85313aea
Luis Salas <luisfernandosalasg@gmail.com>
2020-11-26 05:57:25 -0500

    -

--- properNounCorrection/properNounCorrection.ts
+++ properNounCorrection/properNounCorrection.ts
@@ -1,6 +1,9 @@
 function properNounCorrection(noun: string): string {
-   
+	const firstChar = noun[0].toUpperCase();
+	const restOfTheWord = noun.slice(1, noun.length).toLowerCase();
+
+	return firstChar.concat(restOfTheWord);
 }
 
-console.log(properNounCorrection('pARiS'));
-console.log(properNounCorrection('John'));
+console.log(properNounCorrection("pARiS"));
+console.log(properNounCorrection("John"));
